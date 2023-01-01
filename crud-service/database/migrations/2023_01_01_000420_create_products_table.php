96ce73732b9a7b174b4f1e5214e99fcf96fa3db5
Mauro Flores <mauroflores8193@gmail.com>
2022-12-31 19:11:06 -0500

    se crea products table

---
+++ crud-service/database/migrations/2023_01_01_000420_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('store_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->string("name");
+            $table->decimal('price');
+            $table->integer('discount')->default(0);
+            $table->integer('amount')->default(0);
+            $table->integer('image');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
