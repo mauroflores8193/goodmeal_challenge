40c77b375489b67beb467df234b7c5ef26134956
Mauro Flores <mauroflores8193@gmail.com>
2023-01-01 12:39:28 -0500

    se configura tailwindcss y vue-svg-loader en frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    './public/**/*.html',
+    './src/**/*.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
---
+++ frontend/vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  chainWebpack: (config) => {
+    config.module.rules.delete("svg");
+    config.module.rule("svg")
+      .test(/\.(svg)(\?.*)?$/)
+      .use("babel-loader")
+      .loader("babel-loader")
+      .end()
+      .use("vue-svg-loader")
+      .loader("vue-svg-loader");
+  },
+};
