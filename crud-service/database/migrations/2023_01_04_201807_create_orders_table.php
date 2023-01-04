4f374815ad862a577266c04ef574520fbf19cfa9
Mauro Flores <mauroflores8193@gmail.com>
2023-01-04 15:50:23 -0500

    se crea orders migration

---
+++ crud-service/database/migrations/2023_01_04_201807_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('store_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->string('delivery_type');
+            $table->date('delivery_date');
+            $table->float('delivery_cost');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
