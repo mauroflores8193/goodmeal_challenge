0997dc7b11c6312aa6f6e8f5c99928b104586efe
Mauro Flores <mauroflores8193@gmail.com>
2023-01-04 23:35:17 -0500

    se implementa testing del Home del frontend

---
+++ frontend/tests/unit/home.spec.js
@@ -0,0 +1,19 @@
+import { createLocalVue } from '@vue/test-utils'
+import VueRouter from 'vue-router'
+import { mount } from '@vue/test-utils'
+import Home from '@/components/pages/Home.vue'
+import flushPromises from 'flush-promises'
+import axios from 'axios'
+import storeList from './../mocks/storeList.json'
+
+jest.mock('axios')
+
+const localVue = createLocalVue()
+localVue.use(VueRouter)
+const router = new VueRouter()
+test('increments value on click', async () => {
+  axios.get.mockResolvedValue({ data: storeList });
+  const tree = mount(Home, { localVue, router })
+  await flushPromises();
+  expect(tree).toMatchSnapshot();
+})
