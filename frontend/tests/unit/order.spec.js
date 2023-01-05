b4138150aaab275be1d9e4d43704ae5d7f9749ab
Mauro Flores <mauroflores8193@gmail.com>
2023-01-05 00:07:47 -0500

    se implementa testing de la vista del listado de ordenes

---
+++ frontend/tests/unit/order.spec.js
@@ -0,0 +1,18 @@
+import { createLocalVue, mount } from '@vue/test-utils'
+import VueRouter from 'vue-router'
+import Orders from '@/components/pages/Orders.vue'
+import flushPromises from 'flush-promises'
+import axios from 'axios'
+import orderList from './../mocks/orderList.json'
+
+jest.mock('axios')
+
+const localVue = createLocalVue()
+localVue.use(VueRouter)
+const router = new VueRouter()
+test('increments value on click', async () => {
+  axios.get.mockResolvedValue({ data: orderList });
+  const tree = mount(Orders, { localVue, router })
+  await flushPromises();
+  expect(tree).toMatchSnapshot();
+})
