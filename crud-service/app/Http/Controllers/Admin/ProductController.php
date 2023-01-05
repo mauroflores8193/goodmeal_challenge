f749357f295bcbcb6ecf58767bb2cc28d524724f
Mauro Flores <mauroflores8193@gmail.com>
2023-01-05 15:26:14 -0500

    se implementa swagger para admin/products

--- crud-service/app/Http/Controllers/Admin/ProductController.php
+++ crud-service/app/Http/Controllers/Admin/ProductController.php
@@ -7,15 +7,62 @@ use App\Models\Product;
 use App\Models\Store;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class ProductController extends Controller {
 
+    /**
+     * @OA\Get(path="/api/admin/stores/{storeId}/products",
+     *     security={ {"sanctum": {} }},
+     *     @OA\Parameter(
+     *          name="storeId",
+     *          in="path",
+     *          @OA\Schema(type="integer"),
+     *          required=true,
+     *          description="ID de la tienda",
+     *          example=1
+     *      ),
+     *     tags={"admin/products"},
+     *     @OA\Response(response=200, description="Lista todos los productos."),
+     *     @OA\Response(response=401, description="Unauthorized")
+     * )
+     */
     public function index(): JsonResponse {
         return response()->json(Product::all());
     }
 
+    /**
+     * @OA\Post(path="/api/admin/stores/{storeId}/products",
+     *     description="Envia como parámetro el ID de una tienda. El usuario autorizado tiene que ser el creador de la tienda",
+     *     security={ {"sanctum": {} }},
+     *     @OA\Parameter(
+     *          name="storeId",
+     *          in="path",
+     *          @OA\Schema(type="integer"),
+     *          required=true,
+     *          description="ID de la tienda",
+     *          example=46
+     *      ),
+     *     tags={"admin/products"},
+     *     @OA\MediaType(mediaType="multipart/form-data"),
+     *     @OA\RequestBody(
+     *       @OA\MediaType(
+     *           mediaType="multipart/form-data",
+     *           @OA\Schema(
+     *               type="object",
+     *               required={"name", "price", "old_price", "amount", "image"},
+     *               @OA\Property(property="name", type="string", example="Producto 1"),
+     *               @OA\Property(property="price", type="number", example="100"),
+     *               @OA\Property(property="old_price", type="number", example="150"),
+     *               @OA\Property(property="amount", type="integer", example="10"),
+     *               @OA\Property(property="image", type="file")
+     *           ),
+     *       )
+     *     ),
+     *     @OA\Response(response=201, description="Datos del producto creado"),
+     *     @OA\Response(response=401, description="Unauthorized")
+     * )
+     */
     public function store(Request $request, $store_id): JsonResponse {
         $store = Store::findOrFail($store_id);
         if($store->user_id !== $request->user()->id) abort(403);
@@ -41,12 +88,59 @@ class ProductController extends Controller {
         return $fileName;
     }
 
+    /**
+     * @OA\Get(path="/api/admin/products/{id}",
+     *     security={ {"sanctum": {} }},
+     *     tags={"admin/products"},
+     *     description="Envia como parámetro el ID de un producto y le retorna sus datos. El usuario autorizado tiene que ser el creador del producto",
+     *     @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          @OA\Schema(type="integer"),
+     *          required=true,
+     *          description="ID del producto",
+     *      ),
+     *     @OA\Response(response=200, description="Muestra datos del producto"),
+     *     @OA\Response(response=401, description="Unauthorized"),
+     *     @OA\Response(response=403, description="Forbidden"),
+     *     @OA\Response(response=404, description="Not Found")
+     * )
+     */
     public function show($id): JsonResponse {
         $product = Product::findOrFail($id);
         $this->authorize('view', $product);
         return response()->json($product);
     }
 
+    /**
+     * @OA\Put(path="/api/admin/products/{id}",
+     *     security={ {"sanctum": {} }},
+     *     tags={"admin/products"},
+     *     @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          @OA\Schema(type="integer"),
+     *          required=true,
+     *          description="ID del producto",
+     *      ),
+     *     @OA\MediaType(mediaType="multipart/form-data"),
+     *     @OA\RequestBody(
+     *       @OA\MediaType(
+     *           mediaType="multipart/form-data",
+     *           @OA\Schema(
+     *               type="object",
+     *               @OA\Property(property="name", type="string", example="Otro nombre"),
+     *               @OA\Property(property="price", type="number", example="25"),
+     *               @OA\Property(property="old_price", type="number", example="30"),
+     *               @OA\Property(property="amount", type="integer", example="1"),
+     *               @OA\Property(property="image", type="file")
+     *           ),
+     *       )
+     *     ),
+     *     @OA\Response(response=204, description="Not content"),
+     *     @OA\Response(response=401, description="Unauthorized")
+     * )
+     */
     public function update(Request $request, $id): JsonResponse {
         $this->authorize('update', Product::findOrFail($id));
         $request->validate([
@@ -60,10 +154,27 @@ class ProductController extends Controller {
         return response()->json(null, 204);
     }
 
+    /**
+     * @OA\Delete(path="/api/admin/products/{id}",
+     *     security={ {"sanctum": {} }},
+     *     tags={"admin/products"},
+     *     description="Envia como parámetro el ID de un producto y se eliminan los datos. El usuario autorizado tiene que ser el creador del producto",
+     *     @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          @OA\Schema(type="integer"),
+     *          required=true,
+     *          description="ID del producto",
+     *      ),
+     *     @OA\Response(response=204, description="Not Content"),
+     *     @OA\Response(response=401, description="Unauthorized"),
+     *     @OA\Response(response=403, description="Forbidden"),
+     *     @OA\Response(response=404, description="Not Found")
+     * )
+     */
     public function destroy($id): JsonResponse {
         $product = Product::findOrFail($id);
         $this->authorize('delete', $product);
-        if ($product->image) Storage::disk('public')->delete("uploads/{$product->image}");
         $product->delete();
         return response()->json(null, 204);
     }
