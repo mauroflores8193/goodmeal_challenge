224738fe8ca82357a040a78956fd69aed46f6a69
Mauro Flores <mauroflores8193@gmail.com>
2023-01-05 14:09:55 -0500

    se implementa swagger para endpoints autenticados

--- crud-service/app/Http/Controllers/Admin/StoreController.php
+++ crud-service/app/Http/Controllers/Admin/StoreController.php
@@ -11,6 +11,14 @@ use Illuminate\Support\Str;
 
 class StoreController extends Controller {
 
+    /**
+     * @OA\Get(path="/api/admin/stores",
+     *     security={ {"sanctum": {} }},
+     *     tags={"Listar tiendas (Usuarios autorizados)"},
+     *     @OA\Response(response=200, description="Lista todas las tiendas."),
+     *     @OA\Response(response=401, description="Unauthorized")
+     * )
+     */
     public function index(): JsonResponse {
         return response()->json(Store::all());
     }
@@ -30,8 +38,8 @@ class StoreController extends Controller {
             'banner' => 'required|max:500'
         ]);
         $data['user_id'] = $request->user()->id;
-        if($request->hasFile('icon')) $data['icon'] = $this->uploadFile($request->file('icon'));
-        if($request->hasFile('banner')) $data['banner'] = $this->uploadFile($request->file('banner'));
+        if ($request->hasFile('icon')) $data['icon'] = $this->uploadFile($request->file('icon'));
+        if ($request->hasFile('banner')) $data['banner'] = $this->uploadFile($request->file('banner'));
         return response()->json(Store::create($data), 201);
     }
 
@@ -41,6 +49,24 @@ class StoreController extends Controller {
         return $fileName;
     }
 
+    /**
+     * @OA\Get(path="/api/admin/stores/{id}",
+     *     security={ {"sanctum": {} }},
+     *     tags={"Mostrar datos de una tienda (Usuarios autorizados)"},
+     *     description="Envia como parámetro el ID de una tienda y le retorna sus datos. El usuario autorizado tiene que ser el creador de la tienda",
+     *     @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          @OA\Schema(type="integer"),
+     *          required=true,
+     *          description="ID de la tienda",
+     *      ),
+     *     @OA\Response(response=200, description="Muestra datos de la tienda"),
+     *     @OA\Response(response=401, description="Unauthorized"),
+     *     @OA\Response(response=403, description="Forbidden"),
+     *     @OA\Response(response=404, description="Not Found")
+     * )
+     */
     public function show($id): JsonResponse {
         $store = Store::findOrFail($id);
         $this->authorize('view', $store);
@@ -58,17 +84,33 @@ class StoreController extends Controller {
             'banner' => 'max:500'
         ]);
         $data = $request->all();
-        if($request->hasFile('icon')) $data['icon'] = $this->uploadFile($request->file('icon'));
-        if($request->hasFile('banner')) $data['banner'] = $this->uploadFile($request->file('banner'));
+        if ($request->hasFile('icon')) $data['icon'] = $this->uploadFile($request->file('icon'));
+        if ($request->hasFile('banner')) $data['banner'] = $this->uploadFile($request->file('banner'));
         Store::where('id', $id)->update($data);
         return response()->json(null, 204);
     }
 
+    /**
+     * @OA\Delete(path="/api/admin/stores/{id}",
+     *     security={ {"sanctum": {} }},
+     *     tags={"Eliminar datos de una tienda (Usuarios autorizados)"},
+     *     description="Envia como parámetro el ID de una tienda y se eliminan los datos. El usuario autorizado tiene que ser el creador de la tienda",
+     *     @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          @OA\Schema(type="integer"),
+     *          required=true,
+     *          description="ID de la tienda",
+     *      ),
+     *     @OA\Response(response=204, description="Not Content"),
+     *     @OA\Response(response=401, description="Unauthorized"),
+     *     @OA\Response(response=403, description="Forbidden"),
+     *     @OA\Response(response=404, description="Not Found")
+     * )
+     */
     public function destroy($id): JsonResponse {
         $store = Store::findOrFail($id);
         $this->authorize('delete', $store);
-        if ($store->icon) Storage::disk('public')->delete("uploads/{$store->icon}");
-        if ($store->banner) Storage::disk('public')->delete("uploads/{$store->banner}");
         $store->delete();
         return response()->json(null, 204);
     }
--- crud-service/app/Http/Controllers/AuthController.php
+++ crud-service/app/Http/Controllers/AuthController.php
@@ -6,8 +6,33 @@ use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Auth;
 
+/**
+ * @OA\Info(title="API Usuarios", version="1.0")
+ *
+ * @OA\Server(url="http://localhost:8000")
+ */
 class AuthController extends Controller {
 
+    /**
+     * @OA\Post(
+     *   path="/api/login",
+     *   tags={"Obtener token de autorización"},
+     *   description="Envia sus credenciales de acceso y obtendrá un token de autorización. Use el token para endpoints protedigos enviando en la cabecera de la petición: 'Authorization: Bearer YOUR_TOKEN'",
+     *   @OA\RequestBody(
+     *       required=true,
+     *       description="The Token Request",
+     *       @OA\JsonContent(
+     *        @OA\Property(property="email",type="string",example="mflores@gmail.com"),
+     *        @OA\Property(property="password",type="string",example="password"),
+     *       )
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="TOKEN",
+     *   ),
+     *   @OA\Response(response=401, description="Credenciales incorrectas.")
+     * )
+     */
     public function login(Request $request) {
         $credentials = $request->validate([
             'email' => ['required', 'email'],
